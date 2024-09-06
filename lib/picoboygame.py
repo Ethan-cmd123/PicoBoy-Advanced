46758f71c1095e45d680e6edcf248faa25f38f44
Ethernet Almond <145302595+Ethan-cmd123@users.noreply.github.com>
2024-09-07 10:02:57 +1200

    Added phys shape library into pba.py soo yipeee :)))))

--- lib/picoboygame.py
+++ lib/picoboygame.py
@@ -5,7 +5,7 @@ import audiocore
 import audiopwmio
 import time
 import ledfx
-
+import math
 import digitalio
 from digitalio import DigitalInOut, Direction, Pull
 import displayio
@@ -29,6 +29,65 @@ MAX_LED_BRIGHTNESS = 65535
 LED_BRIGHTNESS_CORRECTION = 1.4
 
 
+class PhysicsShape:
+    def __init__(self, points, x,y, initvx=0.0, initvy=0.0, initva=0.0, vmax=3,screen=None):
+        color_palette = displayio.Palette(1)
+        color_palette[0] = 0xFFFFFF  # White
+        self.shape=rotated_polygon.RotatedPolygon(pixel_shader=color_palette, points=points, x=x, y=y, rotation=0)
+        self.screen=screen
+        screen.append(self.shape)
+        self.init_x, self.init_y = float(x),float(y)
+        self.x, self.y = float(x),float(y)
+        self.vx,self.vy, self.va = initvx,initvy,initva
+        self.vmax=vmax
+    def rotate(self, angle):
+        self.shape.rotation+=angle
+    def rotation(self, angle):
+        self.shape.rotation=angle
+    def accelerate(self, amount, angle=None):
+        if not angle:
+            angle=self.shape.rotation
+        self.vx = -max(min(self.vx + (math.sin(math.radians(angle)) * amount), self.vmax),-self.vmax)
+        self.vy = -max(min(self.vy - (math.cos(math.radians(angle)) * amount), self.vmax),-self.vmax)
+    def bbox(self, relative=False):
+        minX, minY, maxX, maxY = 128, 64, 0, 0
+        for px,py in self.shape[0].points:
+            if px < minX:
+                minX=px
+            if py < minY:
+                minY=py
+            if px > maxX:
+                maxX=px
+            if py > maxY:
+                maxY=py
+        if relative:
+            return [(minX, minY), (maxX, maxY)]
+        return [(int(self.x)+minX, int(self.y)+minY), (int(self.x)+maxX, int(self.y)+maxY)]
+
+    def collidesWith(self, shape):
+        r1min,r1max=self.bbox()
+        r2min,r2max=shape.bbox()
+        
+        return r1max[0] >= r2min[0] and r1min[0]<=r2max[0] and r1max[1] >= r2min[1] and r1min[1]<=r2max[1]
+    def screenWrap(self):
+        if int(self.x) < 0:
+            self.x = 127
+        if int(self.x) > 127:
+            self.x = 0
+        if int(self.y) < 0:
+            self.y=63
+        if int(self.y) > 63:
+            self.y=0
+        self.updateShapePos()
+    def updatePosition(self):
+        self.x+=self.vx
+        self.y+=self.vy
+        self.updateShapePos()
+        self.rotate(self.va)
+    def updateShapePos(self):
+        self.shape.x=int(self.x - self.init_x)
+        self.shape.y=int(self.y - self.init_y)
+        
 class _Neopixel:
     def __init__(self, pin):
         self.pin = digitalio.DigitalInOut(pin)
